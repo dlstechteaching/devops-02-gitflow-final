0986dda77a08c9eaa6a710422daf9e829dda3f3b
dlstechteaching <dls.tech.teaching@gmail.com>
2022-11-08 20:44:21 +0100

    feat(inventions): adding endpoint for invention and sorting param

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const inventionsRouter = require('./routes/inventions');
+const inventionsRouter = require("./routes/inventions");
+const funFactsRouter = require("./routes/funFacts");
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+    res.send("Hello World!");
 });
 
 app.use(inventionsRouter);
+app.use(funFactsRouter);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
--- src/routes/inventions.js
+++ src/routes/inventions.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const Inventions = require('../models/Invention');
-const keyToUpperCase = require('../services/keyToUpperCase');
-const sortPerCreationDate = require('../services/sortPerCreationDate');
+const express = require("express");
+const Inventions = require("../models/Invention");
+const keyToUpperCase = require("../services/keyToUpperCase");
+const sortPerCreationDate = require("../services/sortPerCreationDate");
 
 const router = express.Router();
 
 const sources = [
-    'https://www.thoughtco.com/20th-century-timeline-1992486',
-    'https://en.wikipedia.org/wiki',
+    "https://www.thoughtco.com/20th-century-timeline-1992486",
+    "https://en.wikipedia.org/wiki",
 ];
 
-router.get('/inventions/:key?', (req, res) => {
+router.get("/inventions/:key?", (req, res) => {
     const inventions = keyToUpperCase(
         Inventions.list(),
-        req.params.key ?? 'author'
+        req.params.key ?? "author"
     );
     res.send({
         inventions,
@@ -21,7 +21,7 @@ router.get('/inventions/:key?', (req, res) => {
     });
 });
 
-router.get('/inventions/sort/:orderType', (req, res) => {
+router.get("/inventions/sort/:orderType", (req, res) => {
     const orderedInventions = sortPerCreationDate(
         Inventions.list(),
         req.params.orderType
